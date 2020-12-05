f0496d244f2ba0a7386d56205e9abff0480b6c62
zoniha <awaawapurin@gmail.com>
2020-12-05 11:28:46 +0900

    trivial

---
+++ Pepsi/script.js
@@ -0,0 +1,8 @@
+let imgSlider = (anything) => {
+    document.querySelector('.pepsi').src = anything;
+};
+
+let changeBgColor = (color) => {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+};
